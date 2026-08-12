a8a6603815e26c6bf13b733466e6911c4eb54c0e
tasnimpushpita1-cmd <tasnimpushpita1@gmail.com>
2026-08-12 22:44:09 +0600

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int original = n;
+        int reverse = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        if (original == reverse)
+            System.out.println(original + " is a Palindrome Number.");
+        else
+            System.out.println(original + " is not a Palindrome Number.");
+        sc.close();
+    }
+}
