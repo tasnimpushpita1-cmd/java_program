019f925c3d10fca926e6558d3b4c360620ee1000
tasnimpushpita1-cmd <tasnimpushpita1@gmail.com>
2026-08-12 22:18:10 +0600

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class PrimeNumber{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean prime = true;
+
+        if (n <= 1) {
+            prime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+
+        if (prime)
+            System.out.println(n + " is a Prime Number.");
+        else
+            System.out.println(n + " is not a Prime Number.");
+
+        sc.close();
+    }
+}
