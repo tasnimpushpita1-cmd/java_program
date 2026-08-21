971d7388861c30984d825f03e4d596538170f8c2
tasnimpushpita1-cmd <tasnimpushpita1@gmail.com>
2026-08-21 06:47:47 +0600

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("Greatest Common Divisor (GCD) = " + a);
+    }
+}
