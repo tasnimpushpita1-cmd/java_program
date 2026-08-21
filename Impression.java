c6c3b109ca8ab3a9cd05ee0bbbd2cee9586d2a49
tasnimpushpita1-cmd <tasnimpushpita1@gmail.com>
2026-08-21 06:56:45 +0600

    Create Impression.java

---
+++ Impression.java
@@ -0,0 +1,13 @@
+class Impression {
+    public static void main(String[] args) {
+        System.out.println(
+            "After completing all the programs, " +
+            "I feel like my brain went through a roller coaster ride. " +
+            "Some concepts were easy, some were completely confusing, " +
+            "and a few made me question my life choices. " +
+            "But somehow, I managed to understand a lot and finish everything. " +
+            "I may not be able to explain everything perfectly yet, " +
+            "but at least I survived the programming journey fo now ig! "
+        );
+    }
+}
